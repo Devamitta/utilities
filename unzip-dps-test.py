65c469ba43054845584b272185442ad60514e834
Devamitta <rusdevamitta@gmail.com>
2022-12-09 22:47:00 +0800

    updates

--- unzip-dps-test.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-today = date.today()
-
-from zipfile import ZipFile
-
-with ZipFile(f'../exporter/share/dps-test.zip', 'r') as zipObj:
-   # Extract all the contents of zip file in current directory
-   zipObj.extractall('/home/deva/Documents/GoldenDict')
