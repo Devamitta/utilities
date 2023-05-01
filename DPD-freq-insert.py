cbd15aa37f299743649ff906527c216e81f1ef05
Devamitta <rusdevamitta@gmail.com>
2023-05-01 15:09:44 +0800

    merged + dps

--- DPD-freq-insert.py
+++ DPD-freq-insert.py
@@ -10,12 +10,16 @@ df_dpd.fillna("")
 df_freq = pd.read_csv("/home/deva/Documents/dps/spreadsheets/frequency-ebt.csv", sep="\t", dtype= str)
 df_freq.fillna("")
 
+df_dps = pd.read_csv("/home/deva/Documents/dps/spreadsheets/dps-full.csv", sep="\t", dtype= str)
+df_dps.fillna("")
 
-df_dpd["id-copy"] = df_dpd["user_id"]
+df_dps = df_dps[['id', 'sbs_class_anki', 'sbs_index']]
+
+df_dpd["id"] = df_dpd["user_id"]
 
 df_dpd["pali_1-copy"] = df_dpd["pali_1"]
 
-df_dpd = df_dpd.drop(['id'], axis=1)
+df_dpd = df_dpd.drop(['user_id'], axis=1)
 
 # removing numbers dpd
 df_dpd['pali_1'] = df_dpd['pali_1'].str.replace('\d+', '')
@@ -25,10 +29,11 @@ df_dpd['pali_1'] = df_dpd['pali_1'].str.replace('.', '')
 
 df_merged = pd.merge(df_dpd, df_freq, how='left')
 
-
 df_merged["pali_1"] = df_merged["pali_1-copy"]
 
 df_merged = df_merged.drop(['pali_1-copy'], axis=1)
 
-df_merged.to_csv("/home/deva/Documents/dps/spreadsheets/dpd-freq.csv", sep="\t", index=None)
+df_merged_2 = pd.merge(df_merged, df_dps, how='left')
+
+df_merged_2.to_csv("/home/deva/Documents/dps/spreadsheets/dpd-freq.csv", sep="\t", index=None)
 
