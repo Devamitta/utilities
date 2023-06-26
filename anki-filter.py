ed20fe732378974df4d02bf08f75c413d24de557
Devamitta <devamitta@sasanarakkha.org>
2023-06-27 07:38:05 +0800

    generate anki from db

--- anki-filter.py
+++ anki-filter.py
@@ -1,17 +1,17 @@
 
 import pandas as pd
 
-df = pd.read_csv("../spreadsheets/dps-full.csv", sep="\t", dtype= str)
+df = pd.read_csv("/home/deva/Documents/dpd-db/csvs/dpd-dps-full.csv", sep="\t", dtype= str)
 df.fillna("", inplace=True)
 
-df_anki = df.drop(['sbs_class', 'count'], axis=1)
+df_anki = df.drop(['pali_2', 'Fin', 'Non IA', 'Family', 'Word Family', 'Family2', 'Non-Root In Comps', 'Cognate', 'Category', 'Link', 'Buddhadatta', 'sbs_class'], axis=1)
 
-print("columns 'sbs_class', 'count' has been dropped")
+# print("columns 'sbs_class', 'count' has been dropped")
 
-# # change ru_meaning
-# test1 = df['pali_1'] != ""
-# filter = test1
-# df.loc[filter, ['ru_meaning']] = ""
+# filter all dps words
+test1 = df_anki['ru_meaning'] != ""
+filter = test1
+df_anki = df_anki.loc[filter]
 
 # # filter all SBS words
 # test2 = df['Fin'].str.contains('s')
@@ -20,5 +20,5 @@ print("columns 'sbs_class', 'count' has been dropped")
 
 
 # save csv
-df_anki.to_csv("../csv-for-anki/dps-anki.csv", sep="\t", index=None)
+df_anki.to_csv("../spreadsheets/dps-full.csv", sep="\t", index=None)
 
